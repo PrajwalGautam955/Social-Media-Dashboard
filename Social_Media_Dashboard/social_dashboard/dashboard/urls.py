a0a2e41293390aa06573808facc9468d510e3afd
Prajwal Gautam <141403909+PrajwalGautam955@users.noreply.github.com>
2025-07-23 20:07:16 +0530

    Update urls.py

--- Social_Media_Dashboard/social_dashboard/dashboard/urls.py
+++ Social_Media_Dashboard/social_dashboard/dashboard/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('post/', views.posts_view, name='post'),
     path('api/fetch-posts/', views.fetch_social_posts, name='fetch_social_posts'),
     path('post/view/<int:post_id>/', views.view_post, name='view_post2'),
-    path('view_post/', views.view_post, name='view_post'),
-    path('create-post/', views.create_post_view, name='create_post'),
+    # path('view_post/', views.view_post, name='view_post'),
+    # path('create-post/', views.create_post_view, name='create_post'),
 
     # ✅ Include social_django URLs both at root and at 'auth/'
     path('', include('social_django.urls', namespace='prajwalsocial')),
