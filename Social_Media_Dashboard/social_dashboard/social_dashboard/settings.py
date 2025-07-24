d4adc969e67e36e8b5245552da1808cc64eaf91f
Prajwal Gautam <141403909+PrajwalGautam955@users.noreply.github.com>
2025-07-24 22:59:11 +0530

    Update settings.py

--- Social_Media_Dashboard/social_dashboard/social_dashboard/settings.py
+++ Social_Media_Dashboard/social_dashboard/social_dashboard/settings.py
@@ -146,10 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://555cf4cdab50.ngrok-free.app/'  # replace with your actual ngrok domain
-    # 'https://your-ec2-public-dns',     # if you’re using EC2 public URL
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://555cf4cdab50.ngrok-free.app/'  # replace with your actual ngrok domain
+#     # 'https://your-ec2-public-dns',     # if you’re using EC2 public URL
+# ]
 
 
 
